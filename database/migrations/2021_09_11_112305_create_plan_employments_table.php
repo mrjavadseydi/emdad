fe0e8a4d928d33a9718ec345f1d9ee67bc06e3ea
MrJavad <seydi.birjand@gmail.com>
2021-09-11 12:53:19 +0430

    migration name changes

--- database/migrations/2021_09_11_112305_create_plan_employments_table.php
+++ database/migrations/2021_09_11_112305_create_plan_employments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlaEmploymentsTable extends Migration
+class CreatePlanEmploymentsTable extends Migration
 {
     /**
      * Run the migrations.
