52776848473191c516d6ab2323ed3fbcf1ffd09c
MrJavad <seydi.birjand@gmail.com>
2021-09-11 14:46:36 +0430

    sidbar changes

--- resources/views/master/sidebar.blade.php
+++ resources/views/master/sidebar.blade.php
@@ -3,38 +3,116 @@
         data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
-        <li class="nav-item has-treeview">
+        <li class="nav-item ">
             <a href="{{route('panel')}}"
-
                class="nav-link {{ request()->is('panel') ? 'active':''   }} ">
                 <i class="nav-icon fa fa-dashboard "></i>
                 <p>داشبورد </p>
             </a>
         </li>
         <li class="nav-item has-treeview">
-            <a href="{{route('users.index')}}"
-
-               class="nav-link {{ request()->is('panel/users') ? 'active':''   }} ">
-                <i class="nav-icon fa fa-user "></i>
-                <p>کاربران </p>
-            </a>
-        </li>
-        <li class="nav-item has-treeview">
-            <a href="{{route('office.index')}}"
-
-               class="nav-link {{ request()->is('panel/office') ? 'active':''   }} ">
-                <i class="nav-icon fa fa-university "></i>
-                <p>ادارات </p>
-            </a>
-        </li>
-        <li class="nav-item has-treeview">
-            <a href="{{route('plan.index')}}"
-
-               class="nav-link {{ request()->is('panel/plan') ? 'active':''   }} ">
+            <a href="#"
+               class="nav-link  {{ request()->is('panel/plan') ? 'active':''   }} "
+               >
                 <i class="nav-icon fa fa-map "></i>
-                <p>طرح ها </p>
+                <p>پرونده
+                    <i class="fa fa-angle-left right"></i>
+                </p>
             </a>
+            <ul class="nav nav-treeview" >
+                <li class="nav-item">
+                    <a href="{{route('plan.create')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>ایجاد پرونده</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('plan.index')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>پرونده ها</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('plan.index')}}?my=true" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>پرونده های ارجاع شده</p>
+                    </a>
+                </li>
+            </ul>
+
         </li>
+        <li class="nav-item has-treeview">
+            <a  href="#"
+                class="nav-link {{ request()->is('panel/office') ? 'active':''   }} ">
+                <i class="nav-icon fa fa-university "></i>
+                <p>ادارات
+                    <i class="fa fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview" >
+                <li class="nav-item">
+                    <a href="{{route('office.create')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>ایجاد اداره</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('office.index')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>لیست ادارات</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item has-treeview">
+            <a  href="#"
+                class="nav-link {{ request()->is('panel/office') ? 'active':''   }} ">
+                <i class="nav-icon fa fa-money "></i>
+                <p>بانک ها
+                    <i class="fa fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview" style="display: none;">
+                <li class="nav-item">
+                    <a href="{{route('office.create')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>ایجاد بانک</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('office.index')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>لیست بانک ها</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="nav-item has-treeview">
+            <a  class="nav-link" href="#"
+
+                class="nav-link {{ request()->is('panel/users') ? 'active':''   }} ">
+                <i class="nav-icon fa fa-user "></i>
+                <p>کاربران
+                    <i class="fa fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview" style="display: none;">
+                <li class="nav-item">
+                    <a href="{{route('users.create')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>ایجاد کاربر</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('users.index')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>لیست کاربران</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
     </ul>
 
 </nav>
